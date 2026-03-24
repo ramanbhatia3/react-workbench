357136b26f6deadefb24324689106cdf71ed1906
ramanbhatia3 <bhattiramandeep3105@gmail.com>
2026-03-24 23:47:21 +0530

    counter interview question

---
+++ 06_counter_interviewq/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(5)
+
+  // let counter = 5
+
+  const addValue = () => {
+    // setCounter(counter + 1)
+    // setCounter(counter + 1)
+    // setCounter(counter + 1)
+    // setCounter(counter + 1)
+
+    // setCounter(prevCounter => prevCounter + 1)
+    // setCounter(prevCounter => prevCounter + 1)
+    // setCounter(prevCounter => prevCounter + 1)
+    // setCounter(prevCounter => prevCounter + 1)
+
+    setCounter(counter => counter + 1)
+    setCounter(counter => counter + 1)
+    setCounter(counter => counter + 1)
+    setCounter(counter => counter + 1)
+    setCounter(counter => counter + 1)
+  }
+
+  const removeValue = () => {
+    setCounter(counter - 1)
+  }
+
+
+  return (
+    <>
+      <h1>Counter</h1>
+      <h2>Counter Value: {counter}</h2>
+      <br />
+      <button onClick={addValue}>Add Value</button>
+      <button onClick={removeValue}>Remove Value</button>
+    </>
+  )
+}
+
+export default App
