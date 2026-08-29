59b2ce09695374e4df03c264c62fc3b5663f2a09
ramanbhatia3 <bhattiramandeep3105@gmail.com>
2026-08-29 20:15:04 +0530

    theme toggler

---
+++ 12_theme_switcher/src/components/ThemeBtn.jsx
@@ -0,0 +1,58 @@
+import useTheme from "../context/theme";
+
+export default function ThemeBtn() {
+  const { themeMode, lightTheme, darkTheme } = useTheme();
+
+  const onChangeBtn = (e) => {
+    const darkModeStatus = e.currentTarget.checked;
+
+    if (darkModeStatus) {
+      darkTheme();
+    } else {
+      lightTheme();
+    }
+  };
+
+  return (
+    <label className="relative inline-flex items-center cursor-pointer">
+      <input
+        type="checkbox"
+        className="sr-only peer"
+        onChange={onChangeBtn}
+        checked={themeMode === "dark"}
+      />
+
+      <div
+        className="
+          w-11 h-6
+          bg-gray-200
+          rounded-full
+          peer
+          peer-focus:outline-none
+          peer-focus:ring-4
+          peer-focus:ring-blue-300
+          dark:bg-gray-700
+          dark:peer-focus:ring-blue-800
+          peer-checked:bg-blue-600
+          after:content-['']
+          after:absolute
+          after:top-[2px]
+          after:left-[2px]
+          after:bg-white
+          after:border-gray-300
+          after:border
+          after:rounded-full
+          after:h-5
+          after:w-5
+          after:transition-all
+          peer-checked:after:translate-x-full
+          peer-checked:after:border-white
+        "
+      ></div>
+
+      <span className="ml-3 text-sm font-medium text-gray-900 dark:text-white">
+        Toggle Theme
+      </span>
+    </label>
+  );
+}
